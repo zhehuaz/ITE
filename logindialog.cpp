c79d952fdc3d08f64ccb4c148e05e62af696c2f6
Zhehua Zhang <zhehuaxiao@gmail.com>
2017-01-06 17:48:47 +0800

    Conflict fix

--- logindialog.cpp
+++ logindialog.cpp
@@ -1,21 +1,12 @@
 #include "logindialog.h"
 #include "ui_logindialog.h"
-#include <QDir>
-#include <QDebug>
 
 LoginDialog::LoginDialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::LoginDialog)
 {
     ui->setupUi(this);
-<<<<<<< HEAD
     ui->lineEdit_password->setEchoMode(QLineEdit::Password);
-=======
-    QString fileName;
-    QDir dir;
-    fileName = dir.currentPath();
-    qDebug() << fileName;
->>>>>>> e0d8c92cf47f9da0b2bf323627de982a918e74a2
 }
 
 LoginDialog::~LoginDialog()
